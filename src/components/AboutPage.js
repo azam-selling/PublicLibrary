a0a347005d8f019c82be0b86707fb02d3f52fefe
Azam Ali <azali@ikea.com>
2021-07-17 14:31:21 +0200

    Client module

---
+++ src/components/AboutPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AboutPage() {
+  return (
+    <div className="jumbotron">
+      <h2>About us</h2>
+      <p>This website is to manage library.</p>
+    </div>
+  );
+}
+
+export default AboutPage;
